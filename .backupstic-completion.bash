d40877f9c2dccae55dcd116cf5f8f001db95f449
tricarte <sonagirbas@gmail.com>
2023-03-04 11:02:21 +0300

    sub_go now works with single profile

--- .backupstic-completion.bash
+++ .backupstic-completion.bash
@@ -3,23 +3,22 @@
 # https://stackoverflow.com/questions/5302650/multi-level-bash-completion
 
 # have wpsite &&
-_backupstic_complete()
-{
+_backupstic_complete() {
   local cur prev
 
   COMPREPLY=()
   cur=${COMP_WORDS[COMP_CWORD]}
-  prev=${COMP_WORDS[COMP_CWORD-1]}
+  prev=${COMP_WORDS[COMP_CWORD - 1]}
 
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "backup forget create edit init go" -- $cur) )
+    COMPREPLY=($(compgen -W "backup forget create edit init go" -- $cur))
   elif [ "$COMP_CWORD" -eq 2 ]; then
     case "$prev" in
-      "backup"|"forget"|"edit"|"init")
-          COMPREPLY=($( compgen -W "$(find "$HOME/.config/rustic/" -type f -name '*.toml' -exec basename {} .toml \;)" -- $cur ) )
-        ;;
-      *)
-        ;;
+    "backup" | "forget" | "edit" | "init" | "go")
+      COMPREPLY=($(compgen -W "$(find "$HOME/.config/rustic/" -type f -name '*.toml' -exec basename {} .toml \;)" -- $cur))
+      ;;
+    *) ;;
+
     esac
   fi
 
